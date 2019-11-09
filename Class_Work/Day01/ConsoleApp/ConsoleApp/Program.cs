884297e34f10526bcaaac81d47e1125dfa10b9db
Tasmia Tasmin <tasmintasmia@gmail.com>
2019-11-09 23:42:15 +0600

    Simple Console Application

---
+++ Class_Work/Day01/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Wellcome to Class [" + name + "]");
+            Console.ReadKey();
+			
+			
+			//Console.WriteLine("What is your Name? ");
+            //Console.WriteLine("Hello: " + Console.ReadLine());
+            //Console.ReadKey();
+			
+			//	or
+			
+            //Console.WriteLine("What is your Name? ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Hello: " + name);
+            //Console.ReadKey();
+        }
+    }
+}
