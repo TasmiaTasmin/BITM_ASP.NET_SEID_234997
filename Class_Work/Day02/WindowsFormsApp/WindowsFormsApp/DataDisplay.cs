b10a5a534fc06450caf6cc52b32b768810e81161
Tasmia Tasmin <tasmintasmia@gmail.com>
2019-11-10 00:16:26 +0600

    WindowsFormsApp

---
+++ Class_Work/Day02/WindowsFormsApp/WindowsFormsApp/DataDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class DataDisplay : Form
+    {
+        public DataDisplay()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            int firstNumber;
+            firstNumber = 10;
+
+            int secondNumber = firstNumber;
+            double thirdNumber = secondNumber;
+
+            int fourthNumber = Convert.ToInt32(thirdNumber);
+            string number = fourthNumber.ToString();
+
+            MessageBox.Show("FirstNumber : " + firstNumber + "\nSecondNumber : " + secondNumber + "\nThirdNumber : " + thirdNumber + "\nFourthNumber" + fourthNumber);
+        }
+    }
+}
+
